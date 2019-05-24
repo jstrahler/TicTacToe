5f8d8811268ecd5727742de43b9f0c042e84b909
Nightt Fury <Nightt Fury@Asterisk>
2019-05-24 12:18:59 -0400

    Reset board code

--- TicTacToe/src/tictactoe/Board.java
+++ TicTacToe/src/tictactoe/Board.java
@@ -9,7 +9,9 @@ public class Board {
     }
     
     public static String[] reset(String[] board){
-        
+        for (int i = 0; i < 9; i++){
+            board[i] = Integer.toString(i+1);
+        }
         return board;
     }
     
