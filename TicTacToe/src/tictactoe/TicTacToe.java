370728a7e8a9e61a36b1b16ce7c8c33d86d89ea5
Nightt Fury <Nightt Fury@Asterisk>
2019-05-24 12:25:02 -0400

    Number of players check

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -1,14 +1,30 @@
 
 package tictactoe;
 
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static String[] board = new String[9];
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        String input;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        
+        while (true){
+            System.out.println("How many players are playing, 1 or 2?");
+            input = scan.nextLine().toLowerCase();
+            
+            if (input.equals("1")){
+                
+                break;
+            } else if (input.equals("2")){
+                break;
+            } else {
+                System.out.println("\nPlease enter a valid number of players...\n");
+            }
+        }
     }
     
 }
