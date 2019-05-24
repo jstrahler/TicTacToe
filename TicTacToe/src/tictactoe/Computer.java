ba30b74d5ce2802c0f41b79eec32c7790ed8ffb0
Nightt Fury <Nightt Fury@Asterisk>
2019-05-24 13:57:53 -0400

    Level 1 complete

--- TicTacToe/src/tictactoe/Computer.java
+++ TicTacToe/src/tictactoe/Computer.java
@@ -2,6 +2,7 @@
 package tictactoe;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class Computer {
     
@@ -29,6 +30,49 @@ public class Computer {
         
  
     }
+    
+    
+    public static int compMove(String[] board){
+        int move = 0;
+        Random rand = new Random();
+        
+        if (diff == 1){
+            while (true){
+               move = (int)rand.nextInt(8);
+               if (board[move] != "X" && board[move] != "O"){
+                   break;
+               }
+            }
+            
+        } else if (diff == 2){
+            if (board[0] == "X" && board[2] == "X" && board[1] != "O"){
+                move = 1;
+            }
+            
+            
+            
+            
+            
+            
+            
+        } else if (diff == 3){
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        return move;
+    }
+    
+    
+    
+    
+    
     
     
     public static void p(String x){
