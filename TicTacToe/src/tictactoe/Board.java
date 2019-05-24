1af2d24d4246ccc9351b9a57f7352b58088675f2
Nightt Fury <Nightt Fury@Asterisk>
2019-05-24 12:15:59 -0400

    stuffs

--- TicTacToe/src/tictactoe/Board.java
+++ TicTacToe/src/tictactoe/Board.java
@@ -4,4 +4,13 @@ package tictactoe;
 
 public class Board {
     
+    public static void display(String[] board){
+        
+    }
+    
+    public static String[] reset(String[] board){
+        
+        return board;
+    }
+    
 }
